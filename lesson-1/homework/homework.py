7b63f66c4d756ca5679931b4504f644bea137664
Asadullo-17 <abduvohidovasadullo1707@gmail.com>
2025-05-03 17:14:09 +0500

    Create homework.py

---
+++ lesson-1/homework/homework.py
@@ -0,0 +1,23 @@
+#Lesson-1
+
+#1-task
+a=5
+print('the perimeter of the square is :',a*4)
+print('area of the square is:',a**2)
+
+#2-task
+d=5
+print('the length of the circle is:',d*3.14)
+
+#3-task
+a=2
+b=3
+print((a+b)/2)
+
+#4-task
+a=2
+b=3
+print('the sum of a and b is :',a+b)
+print('the product of  a and b is:',a*b)
+print('the square of a is:',a**2)
+print('the square of b is:',b**2)
